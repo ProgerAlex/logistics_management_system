c0f26b90120ea22611cbf90c9947664d6446a8f5
manankov_albert <modwert404@gmail.com>
2025-01-22 21:48:35 +0700

    connect postgres db

---
+++ src/main/java/com/alex/logistics/logistics_management/LogisticsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.alex.logistics.logistics_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsManagementApplication.class, args);
+	}
+
+}
