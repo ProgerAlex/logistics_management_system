25ae1fe5dff2349fe26f894b0ecb9c5b03fe2569
manankov_albert <modwert404@gmail.com>
2025-01-28 15:00:32 +0700

    add annotation @Transactional to methods in ShipmentRequestService

--- src/main/java/com/alex/logistics/logistics_management/service/ShipmentRequestService.java
+++ src/main/java/com/alex/logistics/logistics_management/service/ShipmentRequestService.java
@@ -3,6 +3,7 @@ package com.alex.logistics.logistics_management.service;
 import com.alex.logistics.logistics_management.model.ShipmentRequest;
 import com.alex.logistics.logistics_management.repository.ShipmentRequestRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,18 +16,22 @@ public class ShipmentRequestService {
         this.repository = repository;
     }
 
+    @Transactional
     public List<ShipmentRequest> getAllRequests() {
         return repository.findAll();
     }
 
+    @Transactional
     public Optional<ShipmentRequest> findById(Long id) {
         return repository.findById(id);
     }
 
+    @Transactional
     public ShipmentRequest createRequest(ShipmentRequest request) {
         return repository.save(request);
     }
 
+    @Transactional
     public void deleteRequest(Long id) {
         repository.deleteById(id);
     }
